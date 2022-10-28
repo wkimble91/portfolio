1db2bdad626d8bb3d8a523f8ce15825b7d925839
will <wkimble91@protonmail.com>
2022-10-28 14:55:22 -0500

    added hamburger menu on mobile

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,9 @@
-function() {
-    $(document).scroll(function () {
-        var $nav=$("#header");
-        $nav.toggleClass("scrolled", $(this).scrollTop()>$nav.height());
-    });
+const hamburger = document.querySelector('.header__hamburger');
+const navMenu = document.querySelector('.header__nav');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
 }
