0588c52aae2d9863d4b120a565011c211f353c79
will <wkimble91@protonmail.com>
2022-11-25 19:00:58 -0600

    added animations to cards

---
+++ js/reveal.js
@@ -0,0 +1,29 @@
+function revealRight() {
+    var reveals = document.querySelectorAll('.revealRight');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add('active');
+        }
+    }
+}
+function revealLeft() {
+    var reveals = document.querySelectorAll('.revealLeft');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add('active');
+        }
+    }
+}
+
+window.addEventListener('scroll', revealRight);
+window.addEventListener('scroll', revealLeft);
