1b4ef859054e7a4e293b5d978ffc4d31cde6408f
will <wkimble91@protonmail.com>
2023-01-03 13:29:04 -0600

    rewrote

--- js/reveal.js
+++ js/reveal.js
@@ -24,7 +24,25 @@ function revealLeft() {
         }
     }
 }
-revealLeft();
+function revealFade() {
+    var reveals = document.querySelectorAll('.revealFade');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add('active');
+        }
+    }
+}
+
+// Initial loading of images on Destinations already on screen
 revealRight();
+revealLeft();
+revealFade();
+
 window.addEventListener('scroll', revealRight);
 window.addEventListener('scroll', revealLeft);
+window.addEventListener('scroll', revealFade);
